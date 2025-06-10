4d2a94264361062c3032b774502100b3378a9453
GerardoQuilla <g.quillaluyo@gmail.com>
2025-06-10 17:52:15 -0500

    initial commit

---
+++ src/mqttClient.js
@@ -0,0 +1,26 @@
+import mqtt from "mqtt";
+import { sendToSpring } from "./stompClient.js";
+
+const brokerUrl = "mqtt://mosquitto:1883";
+const topic = "gg/gas";
+
+const client = mqtt.connect(brokerUrl);
+
+client.on("connect", () => {
+  console.log("Conectado a MQTT");
+  client.subscribe(topic, (err) => {
+    if (!err) console.log(`Suscrito a: ${topic}`);
+  });
+});
+
+client.on("message", (topic, message) => {
+  const payload = message.toString();
+  console.log("Mensaje recibido:", payload);
+
+  try {
+    const json = JSON.parse(payload);
+    sendToSpring(json);
+  } catch (err) {
+    console.error("Error al parsear JSON:", err);
+  }
+});
